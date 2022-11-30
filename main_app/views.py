7b8ea24ebd0d8a16c661e0df9aa2b850a5fe13e2
s9001848 <yenwu@ucdavis.edu>
2022-11-29 22:00:25 -0800

    feat:add blog, index, about, contact templates

--- main_app/views.py
+++ main_app/views.py
@@ -3,14 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World.....')
+    return render(request, "index.html")
 
 def about(request):
-    return HttpResponse('About Page')
+    return render(request, 'about.html')
 
 def contact(request):
-    return HttpResponse('Contact Me')
+    return render(request, 'contact.html')
 
 def blog(request):
-    return HttpResponse('Blog Page')
+    return render(request, 'blog.html')
 
