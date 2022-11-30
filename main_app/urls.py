760155fe74c7cbeb65fdf5b6242062681f60bd7d
s9001848 <yenwu@ucdavis.edu>
2022-11-29 21:39:25 -0800

    feat: add views and urls to main_app

---
+++ main_app/urls.py
@@ -0,0 +1,10 @@
+# main_app/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('blog/', views.blog, name='blog'),
+]
---
+++ main_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hello World.....')
+
+def about(request):
+    return HttpResponse('About Page')
+
+def contact(request):
+    return HttpResponse('Contact Me')
+
+def blog(request):
+    return HttpResponse('Blog Page')
+
