d9efd815d5282ff1ff13e6f36a7b76105ec4fb65
s9001848 <yenwu@ucdavis.edu>
2022-11-29 22:47:56 -0800

    feat: add cat data to index

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# temp add Cats class
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name}"
+
+cats = [
+    Cat('Lolo', 'tabby', 'foul little demon', 3),
+    Cat('Sachi', 'tortoise', 'diluted tortoise shell', 0),
+    Cat('Raven', 'black tripod', 'f3 legged cat', 4),
+]
+
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", { 'cats': cats })
 
 def about(request):
     return render(request, 'about.html')
